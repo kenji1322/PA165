56ced1584130a3b0e1765f42a545883be11a2c2d
Maros Grman <xgrman1@nymfe86.fi.muni.cz>
2017-10-24 13:05:23 +0200

    task 03

--- git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageService.java
+++ git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageService.java
@@ -6,48 +6,44 @@ import java.util.Optional;
 
 import cz.fi.muni.carshop.entities.Car;
 import cz.fi.muni.carshop.enums.CarTypes;
+import cz.fi.muni.carshop.exceptions.RequestedCarNotFoundException;
 
 public interface CarShopStorageService {
 
-	/**
-	 * Adds car to storage, creates type section if not existing before,
-	 * exception should be thrown if car price is less than 0
-	 * 
-	 * @param car
-	 *            car to be added
-	 */
-	void addCarToStorage(Car car);
+    /**
+     * Adds car to storage, creates type section if not existing before,
+     * exception should be thrown if car price is less than 0
+     *
+     * @param car car to be added
+     */
+    void addCarToStorage(Car car);
 
-	/**
-	 * Checks storage for a car of given color and type.
-	 * 
-	 * @param color
-	 *            requested car color
-	 * @param type
-	 *            requested car type
-	 * @return {@link Optional} of {@link Car}, empty if car doesn't exist
-	 */
-	Optional<Car> isCarAvailable(Color color, CarTypes type);
+    /**
+     * Checks storage for a car of given color and type.
+     *
+     * @param color requested car color
+     * @param type requested car type
+     * @return {@link Optional} of {@link Car}, empty if car doesn't exist
+     */
+    Optional<Car> isCarAvailable(Color color, CarTypes type);
 
-	/**
-	 * Retrieves all cars of the same type and construction year as requested
-	 * car.
-	 * 
-	 * @param car
-	 *            reference car
-	 * @return {@link List} of {@link Car}s satisfying the condition
-	 */
-	List<Car> getCheaperCarsOfSameTypeAndYear(Car car);
+    /**
+     * Retrieves all cars of the same type and construction year as requested
+     * car.
+     *
+     * @param car reference car
+     * @return {@link List} of {@link Car}s satisfying the condition
+     */
+    List<Car> getCheaperCarsOfSameTypeAndYear(Car car);
 
-	/**
-	 * Attempts to find a specified car in the storage and remove it from
-	 * storage.
-	 * 
-	 * @param car
-	 *            car to be found in storage
-	 * @throw RequestedCarNotFoundException in case car doesn't exist in the
-	 *        storage
-	 */
-	// void sellCar(Car car) throws RequestedCarNotFoundException;
+    /**
+     * Attempts to find a specified car in the storage and remove it from
+     * storage.
+     *
+     * @param car car to be found in storage
+     * @throw RequestedCarNotFoundException in case car doesn't exist in the
+     * storage
+     */
+    void sellCar(Car car) throws RequestedCarNotFoundException;
 
 }
